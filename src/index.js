cced3e0abec74e71bf5cba3ed28672dc26be67a7
Alonsohdesz <luisalonso123421@gmail.com>
2019-06-10 15:23:40 -0600

    Init Repo

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
+
+const container = document.getElementById('root')
+
+
+
+ReactDOM.render(<Card 
+                 title="Tittle Of Card"
+                 description="Learn amazing street workout and calisthenics"
+                 img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                 leftColor="#A74CF2"
+                 rightColor="#617BFB"
+                />,container)
